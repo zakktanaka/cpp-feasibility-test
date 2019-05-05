459e6df46b278f50701fbfde0d551db048347189
zakk tanaka <zakktanaka@yahoo.co.jp>
2019-05-05 14:51:01 +0900

    adapter

--- cpp-feasibility/gtest-v010803/Source.cpp
+++ cpp-feasibility/gtest-v010803/Source.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
 
-TEST(FooTest, MethodBarDoesAbc) {
+TEST(FooTest183, MethodBarDoesAbc) {
 	int num = 23;
 	EXPECT_EQ(23, num);
 }
 
 // Xyz を行う Foo をテストします．
-TEST(FooTest, DoesXyz) {
+TEST(FooTest183, DoesXyz) {
 }
 
 int main(int argc, char **argv) {
